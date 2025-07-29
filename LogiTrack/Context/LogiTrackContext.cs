8f63b73a66470466fc21a7d0bad79989bfa6a3a6
Oylsister <57207701+oylsister@users.noreply.github.com>
2025-07-29 15:17:12 +0700

    Don't forget to include DbSet for OrderItem.

--- LogiTrack/Context/LogiTrackContext.cs
+++ LogiTrack/Context/LogiTrackContext.cs
@@ -7,6 +7,7 @@ namespace LogiTrack.Context
     {
         public DbSet<InventoryItem> InventoryItems { get; set; }
         public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             options.UseSqlite("Data Source=logitrack.db");
