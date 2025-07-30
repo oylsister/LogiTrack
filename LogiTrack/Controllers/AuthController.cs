ddad8483f1993a965febb9ade676017bfc51801d
Oylsister <57207701+oylsister@users.noreply.github.com>
2025-07-30 11:21:31 +0700

    Register function.

---
+++ LogiTrack/Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+using LogiTrack.DTO;
+using LogiTrack.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LogiTrack.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AuthController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto model)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = model.Username,
+                Email = model.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+                return Ok("User registered successfully");
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+
+            return BadRequest(ModelState);
+        }
+    }
+}
---
+++ LogiTrack/DTO/RegisterDto.cs
@@ -0,0 +1,9 @@
+namespace LogiTrack.DTO
+{
+    public class RegisterDto
+    {
+        public required string Username;
+        public required string Email;
+        public required string Password;
+    }
+}
